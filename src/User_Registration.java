ec97b879a5f04e219a8bf006908cc3be4f1f76e6
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-10 10:55:15 +0530

    Added. validated mobile number

--- src/User_Registration.java
+++ src/User_Registration.java
@@ -3,25 +3,69 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User_Registration {
-      public static boolean isValidEmail(String email) {
-        // Regex to check valid Email.
-        //String regex = "^([a-z 0-9].+[a-z 0-9])@([a-z].+)([a-z].+[a-z])$";
-          String regex = ("^(.+)@(.+)$");
-          // Compile the ReGex
+    public static boolean isValidFirstName(String fname) {
+        // Regex to check valid firstname.
+        String regex = "^[A-Z]{1}[a-z]{3,}$";
+        // Compile the ReGex
         Pattern p = Pattern.compile(regex);
-        if (email == null) {
+        if (fname == null) {
             return false;
         }
-        Matcher m = p.matcher(email);
-          // matched the ReGex
+        Matcher m = p.matcher(fname);
+        // matched the ReGex
+        return m.matches();
+    }
+    public static boolean isValidLastName(String lname) {
+        // Regex to check valid lasttname.
+        String regex = "^[A-Z]{1}[a-z]{3,}$";
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+        if (lname == null) {
+            return false;
+        }
+        Matcher m = p.matcher(lname);
+        // matched the ReGex
+        return m.matches();
+    }
+    public static boolean isValidEmail(String email) {
+      // Regex to check valid Email.
+      String regex = ("^(.+)@(.+)$");
+      // Compile the ReGex
+      Pattern p = Pattern.compile(regex);
+      if (email == null) {
+         return false;
+       }
+      Matcher m = p.matcher(email);
+      // matched the ReGex
+      return m.matches();
+    }
+    public static boolean isValidmobNo(String mobNo) {
+        // Regex to check valid Email.
+        String regex = ("^\\+(?:[0-9] ?){6,14}[0-9]$");
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+        if (mobNo == null) {
+            return false;
+        }
+        Matcher m = p.matcher(mobNo);
+        // matched the ReGex
         return m.matches();
     }
 
        public static void main(String[] args) {
             // Test UseCase: 2
            Scanner sc = new Scanner(System.in);
+           System.out.println("Enter First Name:");
+           String fname = sc.nextLine();
+           System.out.println("Enter Last Name:");
+           String lname = sc.nextLine();
            System.out.println("Enter Email:");
-           String str1 = sc.nextLine();
-           System.out.println(isValidEmail(str1));
+           String email = sc.nextLine();
+           System.out.println("Enter Mobile Number:");
+           String mobNo = sc.nextLine();
+           System.out.println(isValidFirstName(fname));
+           System.out.println(isValidLastName(lname));
+           System.out.println(isValidEmail(email));
+           System.out.println(isValidmobNo(mobNo));
         }
 }
